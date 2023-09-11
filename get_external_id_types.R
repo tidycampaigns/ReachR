8097c2146bc121dcda88733ad1d3be87f4121d6c
claytonperry <claytonhperry@gmail.com>
2023-09-10 21:37:09 -0400

    change external id types output

--- get_external_id_types.R
+++ get_external_id_types.R
@@ -5,14 +5,18 @@ get_external_id_types <- function() {
 
     auth_reach()
 
-    VERB(
-
+    external_id_types <<- VERB(
         "GET",
         url = "https://api.reach.vote/api/v1/external-ids",
-        add_headers(
-            "authorization" = paste0("Bearer ", access_token)
-        )
-
+        add_headers("authorization" = paste0("Bearer ", access_token))
+    ) %>% content() %>% list() %>%
+    pluck(1, 1) %>%
+    unlist() %>%
+    as.data.frame(.) %>%
+    rename(.,
+        external_id_type = `.`,
     )
+
+    print("External ID Types Retrieved in dataframe 'external_id_types'")
     
 }
